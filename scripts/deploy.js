3261e55f14d5cbc93c6969c994af15f814a0008e
Adam Yarger <yargeradam@gmail.com>
2021-06-21 20:34:35 -0700

    adds write to json script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,25 @@
 const hre = require("hardhat");
 
 async function main() {
-    const LlamaFactory = await hre.ethers.getContractFactory('LlamaFactory')
-    // in order to pass params to the contract contructor we pass them through deploy
-    const llamaFactory = await LlamaFactory.deploy()
+  const LlamaFactory = await hre.ethers.getContractFactory('LlamaFactory')
+  // in order to pass params to the contract contructor we pass them through deploy
+  const llamaFactory = await LlamaFactory.deploy()
 
-    await llamaFactory.deployed()
+  await llamaFactory.deployed()
 
-    // should make this copy to a file
-    console.log('LlamaFactory deployed to:', llamaFactory.address)
+  // should make this copy to a file
+  console.log('LlamaFactory deployed to:', llamaFactory.address)
+  saveContractAddress('LlamaFactory', llamaFactory)
+}
+
+function saveContractAddress(contract, name) {
+  const fs = requre('fs')
+
+  // this should do Object.assign
+  fs.writeFileSync(
+    __dirname + '..contract-addresses.json',
+    JSON.stringify({ [name]: contract.address }, undefined, 2)
+  )
 }
 
 
@@ -16,8 +27,8 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
