75cdba6b9ad93408c08364092e142a78354d041c
Adam Yarger <yargeradam@gmail.com>
2021-06-24 19:28:17 -0700

    adds count and button to breed page

--- pages/breed/index.tsx
+++ pages/breed/index.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
-import { Box, Container } from '@chakra-ui/layout'
+import React, { useEffect, useState } from 'react'
+import { Box, Container, Button } from '@chakra-ui/react'
+import { useWeb3 } from 'context/AppContext'
+import { ethers } from 'ethers'
+import LlamaFactory from 'artifacts/contracts/LlamaFactory.sol/LlamaFactory.json'
+import contractAddresses from 'contract-addresses.json'
 
 export default function Breed() {
+    const { provider } = useWeb3()
+    const [count, setCount] = useState(0)
+
+    // create hook for getting specific contract objects
+    const llamaFactory = new ethers.Contract(
+        contractAddresses.LlamaFactory,
+        LlamaFactory.abi,
+        provider
+    );
+
+    const disableBreeding = () => {
+        return count < 2
+    }
+
+    const breed = () => {
+        console.log('breeding...')
+    }
+
+    const getCount = async () => {
+        const _count = await llamaFactory.llamaCount()
+        setCount(_count.toNumber())
+    }
+
+    useEffect(() => {
+        if (provider) {
+            getCount()
+        }
+    }, [provider])
+
     return (
         <Container maxW="container.lg" mt="32">
             <Box>
-                Breed me
+                {/* Count: {count} */}
+                <Button colorScheme="purple" disabled={disableBreeding()} onClick={breed}>
+                    Breed Me
+                </Button>
             </Box>
         </Container>
     )
