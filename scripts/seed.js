9841460a7e4bed49b8bc46a419d3dcaeb1f9ce35
Adam Yarger <yargeradam@gmail.com>
2021-06-27 16:00:39 -0700

    adds seeds script

---
+++ scripts/seed.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat")
+const contractAddresses = require("../contract-addresses.json")
+
+const llamas = [
+  {
+    name: 'Mom'
+  }, {
+    name: 'Dad'
+  }
+]
+
+function waitFor(p) {
+  return p.then((tx) => tx.wait())
+}
+
+async function main() {
+  for (let i = 0; i < llamas.length; i++) {
+    const llamaFactory = await ethers.getContractAt(
+      'LlamaFactory',
+      contractAddresses.LlamaFactory
+    )
+    await waitFor(llamaFactory.createRandomLlama(llamas[i].name))
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
